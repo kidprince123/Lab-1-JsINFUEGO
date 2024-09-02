ad1d8a5a7c9520702f9f1b3bde853e4fa9af75f5
Joh <rixityisamazing@Outlook.com>
2024-09-02 00:14:37 -0400

    Initial Commit

---
+++ Lab 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab 1
+//
+//  Created by John Sinfuego on 9/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
